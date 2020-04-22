302e5388bc866601c2a2468f84670aa232c7fb90
Michal Čihař <michal@cihar.com>
2020-04-22 13:14:05 +0200

    Improve setup

--- sync.sh
+++ sync.sh
@@ -2,6 +2,10 @@
 
 REPOS="customize-example wlc scripts fedora_messaging weblate website weblate_schemas translation-finder munin fail2ban docker docker-compose hosted wllegal language-data"
 
+INITFILES="requirements-lint.txt .pre-commit-config.yaml"
+COPYFILES=".github/stale.yml .github/labels.yml .github/workflows/closing.yml .github/workflows/labels.yml .github/workflows/label-sync.yml .github/workflows/pre-commit.yml .github/FUNDING.yml .yamllint.yml"
+PRESENTFILES=".github/matchers/sphinx-linkcheck.json .github/matchers/sphinx.json .github/matchers/flake8.json "
+
 mkdir -p repos
 cd repos
 
@@ -14,11 +18,29 @@ for repo in $REPOS ; do
         git pull -q
     fi
     echo "== $repo =="
+    git reset --hard origin/master
 
     # Check README
     if ! grep -q Logo-Darktext-borders.png README.rst 2>/dev/null ; then
         echo "WARNING: README.rst not containing logo."
     fi
+
+    for file in $INITFILES ; do
+        if [ ! -f $file ] ; then
+            cp ../../$file $file
+        fi
+    done
+    for file in $COPYFILES ; do
+        cp ../../$file $file
+    done
+    for file in $PRESENTFILES ; do
+        if [ -f $file ] ; then
+            cp ../../$file $file
+        fi
+    done
+    git add .
+    git commit -m 'Sync with WeblateOrg/meta'
+    git show
 
     echo
     cd ..
