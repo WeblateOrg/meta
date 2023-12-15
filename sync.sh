013f0f0e6b9723406beee7cd1941f88cb90ad77e
Michal Čihař <michal@cihar.com>
2023-12-15 10:40:10 +0100

    avoid always injecting requirements-lint.txt
    It is not used for pyproject.toml builds.

--- sync.sh
+++ sync.sh
@@ -31,7 +31,6 @@ REPOS="
 
 # Copy the files to all repos if not present, these are expected to diverge
 INITFILES="
-    requirements-lint.txt
     .pre-commit-config.yaml
 "
 
@@ -46,6 +45,7 @@ COPYFILES="
     .github/workflows/pull_requests.yaml
     .github/FUNDING.yml
     .yamllint.yml
+    requirements-lint.txt
     SECURITY.md
     .github/PULL_REQUEST_TEMPLATE.md
     .markdownlint.yml
