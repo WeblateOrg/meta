a9fb323ce4e92d4d0b2870d64ca06c265c64b812
Michal Čihař <michal@cihar.com>
2026-05-07 11:18:52 +0200

    feat(ci): separate pre-commit caching setup
    This makes it easier to reuse.

--- sync.sh
+++ sync.sh
@@ -41,6 +41,7 @@ INITFILES="
 # Copy these files unconditionally
 COPYFILES="
     .github/workflows/pre-commit.yml
+    .github/actions/pre-commit-setup/action.yml
     .github/workflows/pull_requests.yaml
     .github/workflows/dependency-review.yml
     .github/FUNDING.yml
